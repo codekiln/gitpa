817370e4255a2a052fe55a167d659d61bf7bf9cf
Myer Nore <codekiln@pm.me>
2024-12-23 06:11:34 -0500

    docs: update usage of update_file_attrs

--- gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update/file_attrs/update_file_attrs.py
+++ gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update/file_attrs/update_file_attrs.py
@@ -1,7 +1,13 @@
 """
 update_file_attrs.py
-Given a path to a path a the logseq directory for an episode, 
+
+Given a path to a path to the logseq directory for an episode, 
 update the attributes in episode.yml that point to files in that directory.
 
 Uses AI to infer the files.
+
+Usage:
+    update_file_attrs.py <episode_date> - update the file attributes for the given episode date.
+    update_file_attrs.py --reference - update the file attributes for the reference episode.
 """
+
