b799374c499838f5ea2ba2dbdaa32b09a0abf171
Myer Nore <codekiln@pm.me>
2024-12-23 06:56:11 -0500

    feat: update_file_attrs uses new constants for reference episode directory

--- gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update/file_attrs/update_file_attrs.py
+++ gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update/file_attrs/update_file_attrs.py
@@ -12,7 +12,8 @@ Usage:
 """
 
 from gitp_acolyte.ceremonial.spells.episode_data.create import get_episode_dir
-from gitp_acolyte.ceremonial.spells.episode_reference.constants import REFERENCE_EPISODE_DATE
+from gitp_acolyte.ceremonial.spells.episode_reference.constants import REFERENCE_EPISODE_DATE_STR, REFERENCE_EPISODE_DATE
+from gitp_acolyte.constants import DATE_FORMAT, REFERENCE_EPISODE_DIR
 
 def get_args():
     parser = argparse.ArgumentParser(description="Update the file attributes in episode.yml.")
@@ -20,10 +21,16 @@ def get_args():
     parser.add_argument("--reference", action="store_true", help="Update the file attributes for the reference episode.")
     return parser.parse_args()
 
+def get_episode_dir(episode_date: None | datetime = None) -> Path:
+    raise NotImplementedError("TODO - import function from create.py")
+
 def main():
     args = get_args()
     episode_date = args.episode_date
     if args.reference:
         episode_date = REFERENCE_EPISODE_DATE
-    episode_dir = get_episode_dir(episode_date)
+        episode_dir = REFERENCE_EPISODE_DIR
+    else:
+        episode_date = datetime.strptime(episode_date, DATE_FORMAT)
+        episode_dir = get_episode_dir(episode_date)
     update_file_attrs(episode_dir)
--- gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_reference/constants.py
+++ gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_reference/constants.py
@@ -1,1 +1,6 @@
-REFERENCE_EPISODE_DATE = "2024-12-04"
+from datetime import datetime
+from gitp_acolyte.constants import DATE_FORMAT
+
+REFERENCE_EPISODE_DATE_STR = "2024-12-04"
+
+REFERENCE_EPISODE_DATE = datetime.strptime(REFERENCE_EPISODE_DATE_STR, DATE_FORMAT)
