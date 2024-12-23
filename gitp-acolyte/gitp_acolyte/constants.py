6643df88aa9ef65271aaae492754d16bf0f41651
Myer Nore <codekiln@pm.me>
2024-12-23 06:56:11 -0500

    feat: update_file_attrs uses new constants for reference episode directory

--- gitp-acolyte/gitp_acolyte/constants.py
+++ gitp-acolyte/gitp_acolyte/constants.py
@@ -3,6 +3,14 @@ from pathlib import Path
 RECORDINGS_ROOT_FOLDER = Path('~/Documents/ableton/GitP')
 
 REPO_ROOT = Path(__file__).resolve().parent.parent.parent
+
+# gitp-acolyte paths
+ACOLYTE_DIR = REPO_ROOT / 'gitp-acolyte'
+CEREMONIAL_DIR = ACOLYTE_DIR / 'ceremonial'
+SPELLS_DIR = CEREMONIAL_DIR / 'spells'
+REFERENCE_DIR = SPELLS_DIR / 'episode_reference'
+REFERENCE_EPISODE_DIR = REFERENCE_DIR / 'ref_ep_dir'
+
 LOGSEQ_FOLDER = REPO_ROOT / 'gitp-garden'
 LOGSEQ_ASSETS_FOLDER = LOGSEQ_FOLDER / 'assets'
 LOGSEQ_PAGES_FOLDER = LOGSEQ_FOLDER / 'pages'
