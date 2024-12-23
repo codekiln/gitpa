0d40fe19a07a091b6a54c6aa652dc54ffad264e9
Myer Nore <codekiln@pm.me>
2024-12-23 05:48:28 -0500

    refactor: move episode_data/update.py to episode_data/update/update_file_attrs.py

--- gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update.py
+++
@@ -1,3 +0,0 @@
-"""
-The purpose of 
-"""
---
+++ gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update/file_attrs/update_file_attrs.py
@@ -0,0 +1,7 @@
+"""
+update_file_attrs.py
+Given a path to a path a the logseq directory for an episode, 
+update the attributes in episode.yml that point to files in that directory.
+
+Uses AI to infer the files.
+"""
