6afe20acd1b56a32edd474394193fefa3aefe71a
Myer Nore <codekiln@pm.me>
2024-12-23 06:27:23 -0500

    feat: add Makefile command to update file attributes for the reference episode

--- gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update/file_attrs/update_file_attrs.py
+++ gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update/file_attrs/update_file_attrs.py
@@ -11,3 +11,19 @@ Usage:
     update_file_attrs.py --reference - update the file attributes for the reference episode.
 """
 
+from gitp_acolyte.ceremonial.spells.episode_data.create import get_episode_dir
+from gitp_acolyte.ceremonial.spells.episode_reference.constants import REFERENCE_EPISODE_DATE
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Update the file attributes in episode.yml.")
+    parser.add_argument("episode_date", type=str, help="The episode date in the format YYYY-MM-DD.")
+    parser.add_argument("--reference", action="store_true", help="Update the file attributes for the reference episode.")
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    episode_date = args.episode_date
+    if args.reference:
+        episode_date = REFERENCE_EPISODE_DATE
+    episode_dir = get_episode_dir(episode_date)
+    update_file_attrs(episode_dir)
