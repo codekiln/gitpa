3bdf18415a320ec0a09c85bc6d52e1002039f407
Myer Nore <codekiln@pm.me>
2024-12-25 04:43:18 -0500

    feat: add logging to update_file_attrs and enhance main function for better debugging

--- gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update/file_attrs/update_file_attrs.py
+++ gitp-acolyte/gitp_acolyte/ceremonial/spells/episode_data/update/file_attrs/update_file_attrs.py
@@ -16,14 +16,31 @@ from datetime import datetime
 from gitp_acolyte.ceremonial.spells.episode_data.args import define_common_args, get_episode_date
 from gitp_acolyte.constants import DATE_FORMAT, REFERENCE_EPISODE_DIR
 from gitp_acolyte.ceremonial.spells.episode_data.create import ensure_episode_dir_and_yaml_exists
+import logging
+import coloredlogs
+
+# Configure logging
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=logger, fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def get_args():
     parser = define_common_args()
     return parser.parse_args()
 
 
+def update_file_attrs(episode_dir, args):
+    raise NotImplementedError("This function is not implemented yet.")
+
+
+
 def main():
     args = get_args()
     episode_date = get_episode_date(args)
+    logger.debug(f"Episode date: {episode_date}")
     episode_dir, _ = ensure_episode_dir_and_yaml_exists(episode_date, args)
-    update_file_attrs(episode_dir)
+    logger.debug(f"Episode directory: {episode_dir}")
+    update_file_attrs(episode_dir, args)
+    logger.info("File attributes updated successfully.")
+
+if __name__ == "__main__":
+    main()
